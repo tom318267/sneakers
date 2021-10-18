28a588cb2d545a6573bace51eb92ee4bbb4e8b89
tom318267 <ogar318267@gmail.com>
2021-10-18 14:29:32 -0400

    adjust card component

--- pages/products/index.js
+++ pages/products/index.js
@@ -17,9 +17,8 @@ const ProductsPage = () => {
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {DATA.map((product) => (
               <div
-                onClick={() => addToCart(product)}
                 key={product.id}
-                className="group relative p-8"
+                className="group item-container relative p-8"
               >
                 <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden lg:h-80 lg:aspect-none">
                   <img
@@ -30,20 +29,26 @@ const ProductsPage = () => {
                 </div>
                 <div className="mt-4 flex justify-between">
                   <div>
-                    <h3 className="text-sm text-black font-bold">
+                    <h3 className="text-lg sm:text-sm text-black font-bold">
                       <a href={product.href}>
                         <span aria-hidden="true" className="absolute inset-0" />
                         {product.name}
                       </a>
                     </h3>
-                    <p className="mt-1 text-sm text-gray-500">
+                    <p className="mt-1 text-lg sm:text-sm text-gray-500">
                       {product.brand}
                     </p>
                   </div>
-                  <p className="text-sm text-black font-bold">
-                    {product.price}
+                  <p className="text-lg sm:text-sm text-black font-bold">
+                    ${product.price}
                   </p>
                 </div>
+                <button
+                  className="relative z-50 bg-black text-white text-sm py-2 px-4 rounded-full float-right hover:shadow-xl active:scale-90 transition duration-150"
+                  onClick={() => addToCart(product)}
+                >
+                  Add to Cart
+                </button>
               </div>
             ))}
           </div>
