fcd9cb7d7e4a2e7a77f72532415eb9e3923828e2
tom318267 <ogar318267@gmail.com>
2021-10-18 15:00:09 -0400

    adjust cartdropdown

--- components/CartDropdown.js
+++ components/CartDropdown.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../context/CartContext";
 import CartItem from "./CartItem";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className="CartDropdown absolute w-[300px] h-[300px] overflow-scroll bg-white top-20 right-0 z-10 rounded-lg text-sm">
-      <CartItem />
+      <CartItem cartItems={cartItems} />
     </div>
   );
 };
--- components/CartItem.js
+++ components/CartItem.js
@@ -2,31 +2,37 @@ import React, { useContext } from "react";
 import { FaTrashAlt } from "react-icons/fa";
 import CartContext from "../context/CartContext";
 
-const CartItem = () => {
-  const { cartItems, removeItemFromCart } = useContext(CartContext);
+const CartItem = ({ cartItems }) => {
+  const { removeItemFromCart } = useContext(CartContext);
 
-  return cartItems.map((item) => (
-    <div
-      key={item.id}
-      className="flex w-full items-center justify-center gap-21 p-5"
-    >
-      <img className="w-[20%] rounded-md" src={item.imageSrc} alt="" />
-      <div className="w-[80%] gap-1 flex flex-col items-center">
-        <div className="w-[200px]">
-          <span>{item.name}</span>
+  return cartItems.length ? (
+    cartItems.map((item) => (
+      <div
+        key={item.id}
+        className="flex w-full items-center justify-center gap-21 p-5"
+      >
+        <img className="w-[20%] rounded-md" src={item.imageSrc} alt="" />
+        <div className="w-[80%] gap-1 flex flex-col items-center">
+          <div className="w-[200px]">
+            <span>{item.name}</span>
+          </div>
+
+          <div className="flex gap-2">
+            <span className="text-gray-400">${item.price} x 1</span>
+            <span className="text-black text-sm font-bold">${item.price}</span>
+          </div>
         </div>
 
-        <div className="flex gap-2">
-          <span className="text-gray-400">${item.price} x 1</span>
-          <span className="text-black font-bold">${item.price}</span>
+        <div onClick={() => removeItemFromCart(item)}>
+          <FaTrashAlt />
         </div>
       </div>
-
-      <div onClick={() => removeItemFromCart(item)}>
-        <FaTrashAlt />
-      </div>
-    </div>
-  ));
+    ))
+  ) : (
+    <h1 className="flex text-black h-full justify-center items-center">
+      Your cart is empty
+    </h1>
+  );
 };
 
 export default CartItem;
